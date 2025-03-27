b18e44702d7245abf1affb280bb4848efc2e702e
eren-erol <erenerolgh@gmail.com>
2025-03-27 20:01:15 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f60dbd72-b07e-4bf3-8776-2a9f8098278a';
+export const MODAL_CLOSE_SEC = 2.5;
