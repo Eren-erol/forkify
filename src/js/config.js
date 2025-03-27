a04687d329dcb111bb5bdcf63b0db5ed073b7443
eren-erol <erenerolgh@gmail.com>
2025-03-27 20:48:26 +0300

    API Url changed

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'f60dbd72-b07e-4bf3-8776-2a9f8098278a';
